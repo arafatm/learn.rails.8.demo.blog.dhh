bd6a509adc738d635c71e3e046411298053ad751
Arafat M <19172+arafatm@users.noreply.github.com>
2024-11-22 14:47:55 -0800

    Remove raised exception from earlier

--- blog/app/controllers/posts_controller.rb
+++ blog/app/controllers/posts_controller.rb
@@ -4,7 +4,6 @@ class PostsController < ApplicationController
   # GET /posts or /posts.json
   def index
     @posts = Post.all
-    raise "some exception"
   end
 
   # GET /posts/1 or /posts/1.json
