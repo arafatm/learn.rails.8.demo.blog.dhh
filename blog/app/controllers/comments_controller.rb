d92338f3ecfce5cc518df70e08214c93fe5fcf9e
Arafat M <19172+arafatm@users.noreply.github.com>
2024-11-25 16:09:48 -0800

    Post comments and form to add a comment

--- blog/app/controllers/comments_controller.rb
+++ blog/app/controllers/comments_controller.rb
@@ -1,2 +1,14 @@
 class CommentsController < ApplicationController
+  before_action :set_post # see prvate method below
+
+  def create
+    @post.comments.create! params.expect(comment: [ :content ])
+    redirect_to @post
+  end
+
+  private
+  # Comment is a child of Post, so we need to find the parent Post
+  def set_post
+    @post = Post.find(params[:post_id])
+  end
 end
